27fc5e98b628c79b9b3684181d66761b0d0094c8
tiho <tiho1012@gmail.com>
2013-02-05 23:39:39 -0500

    FirstApp: Initial Setup  On branch master
     Initial commit
    
    	new file:   .gitignore
    	new file:   Gemfile
    	new file:   Gemfile.lock
    	new file:   README.rdoc
    	new file:   Rakefile
    	new file:   app/assets/images/rails.png
    	new file:   app/assets/javascripts/application.js
    	new file:   app/assets/stylesheets/application.css
    	new file:   app/controllers/application_controller.rb
    	new file:   app/helpers/application_helper.rb
    	new file:   app/mailers/.gitkeep
    	new file:   app/models/.gitkeep
    	new file:   app/views/layouts/application.html.erb
    	new file:   config.ru
    	new file:   config/application.rb
    	new file:   config/boot.rb
    	new file:   config/database.yml
    	new file:   config/environment.rb
    	new file:   config/environments/development.rb
    	new file:   config/environments/production.rb
    	new file:   config/environments/test.rb
    	new file:   config/initializers/backtrace_silencers.rb
    	new file:   config/initializers/inflections.rb
    	new file:   config/initializers/mime_types.rb
    	new file:   config/initializers/secret_token.rb
    	new file:   config/initializers/session_store.rb
    	new file:   config/initializers/wrap_parameters.rb
    	new file:   config/locales/en.yml
    	new file:   config/routes.rb
    	new file:   db/seeds.rb
    	new file:   doc/README_FOR_APP
    	new file:   lib/assets/.gitkeep
    	new file:   lib/tasks/.gitkeep
    	new file:   log/.gitkeep
    	new file:   public/404.html
    	new file:   public/422.html
    	new file:   public/500.html
    	new file:   public/favicon.ico
    	new file:   public/index.html
    	new file:   public/robots.txt
    	new file:   script/rails
    	new file:   test/fixtures/.gitkeep
    	new file:   test/functional/.gitkeep
    	new file:   test/integration/.gitkeep
    	new file:   test/performance/browsing_test.rb
    	new file:   test/test_helper.rb
    	new file:   test/unit/.gitkeep
    	new file:   vendor/assets/javascripts/.gitkeep
    	new file:   vendor/assets/stylesheets/.gitkeep
    	new file:   vendor/plugins/.gitkeep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e70553d4d3628e0e882be1fba6561a77f42f9646e31c9c2fedad35d22d0736048fc8bffd23c13bdca32746c0bcde2c3cde7f03a05d1e19afa81d84f8a4cc8648'
